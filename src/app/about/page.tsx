f5001a113fc00481d924d54b6bd37e71857471a7
Francisco Marcano <fjmn2001@gmail.com>
2024-02-08 09:52:51 -0400

    adding shadcn-ui

---
+++ src/app/about/page.tsx
@@ -0,0 +1,17 @@
+// export const AboutPage = () => {
+// 	return (
+// 		<>
+// 			<h1>About</h1>
+// 			<hr />
+// 		</>
+// 	);
+// };
+
+export default function AboutPage() {
+	return (
+		<>
+			<h1>About Page</h1>
+			<hr />
+		</>
+	);
+}
