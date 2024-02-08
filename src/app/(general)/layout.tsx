cde96c900ffa657379051ddaf3c3ed0f7698bdd1
Francisco Marcano <fjmn2001@gmail.com>
2024-02-08 12:39:56 -0400

    adding navbar

--- src/app/(general)/layout.tsx
+++ src/app/(general)/layout.tsx
@@ -1,11 +1,16 @@
 import { ReactNode } from "react";
 
+import { Navbar } from "@/sections/shared/ui/navbar";
+
 export default function GeneralLayout({ children }: { children: ReactNode }) {
 	return (
-		<main className={"flex flex-col items-center p-24"}>
-			<span className={"text-lg"}>Section</span>
-			{children}
-			<hr />
-		</main>
+		<>
+			<Navbar />
+			<main className={"flex flex-col items-center p-24"}>
+				<span className={"text-lg"}>Section</span>
+				{children}
+				<hr />
+			</main>
+		</>
 	);
 }
---
+++ src/sections/shared/ui/navbar.tsx
@@ -0,0 +1,19 @@
+export const Navbar = () => {
+	return (
+		<div className={"flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded"}>
+			<span>Home</span>
+
+			<div className={"flex-1"}></div>
+
+			<a className={"mr-2"} href={"/about"}>
+				About
+			</a>
+			<a className={"mr-2"} href={"/pricing"}>
+				Pricing
+			</a>
+			<a className={"mr-2"} href={"/contact"}>
+				Contact
+			</a>
+		</div>
+	);
+};
